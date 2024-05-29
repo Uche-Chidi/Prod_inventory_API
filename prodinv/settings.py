2f28513e3d751008cad81aa1666c1534d981f67b
Uche-Chidi <geoegeogwara@gmail.com>
2024-05-29 11:45:56 +0100

    add domain name

--- prodinv/settings.py
+++ prodinv/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["prod-inventory-api.onrender.com"]
 
 
 # Application definition
